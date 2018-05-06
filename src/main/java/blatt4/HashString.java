277a4c6d02576d4a26d1aa50b66b28849ed7dd2a
Max Fr <fruehaufmaximilian@gmail.com>
2018-05-06 14:17:17 +0200

    add blatt4 problemset

---
+++ src/main/java/blatt4/HashString.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+/**
+ * Die Klasse {@link HashString} kann dazu verwendet werden,
+ * Strings zu hashen.
+ */
+public class HashString {
+  /*
+   * Todo
+   */
+
+  /**
+   * Dieser Konstruktor initialisiert ein {@link HashString}
+   * Objekt für einen gegebenen Maximalwert (size - 1) der gehashten
+   * Werte.
+   * 
+   * @param size die Größe der Hashtabelle
+   */
+  public HashString (int size) {
+    /*
+     * Todo
+     */
+  }
+
+  /**
+   * Diese Methode berechnet den Hashwert für einen String.
+   * 
+   * @param key der Schlüssel, der gehasht werden sollen
+   * @return der Hashwert des Schlüssels
+   */
+  public int hash (String key) {
+    /*
+     * Todo
+     */
+  }
+}
